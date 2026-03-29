8446977904a6d839ff4d94ea824e2acc765bd7ac
kollusmartphone-rgb <kollu.smartphone@gmail.com>
2026-03-29 18:10:02 +0530

    Update evaluate.js

--- api/evaluate.js
+++ api/evaluate.js
@@ -4,21 +4,39 @@ export default async function handler(req, res) {
   let score = 0;
   let reasons = [];
 
-  // ===== FUNDAMENTALS =====
-  if (pe > 0 && pe < 25) {
+ // ===== FUNDAMENTALS =====
+if (pe > 0) {
+  if (pe < 25) {
     score++;
     reasons.push("Good PE");
+  } else {
+    reasons.push("High PE");
   }
+} else {
+  reasons.push("PE not available");
+}
 
+if (roe > 0) {
   if (roe > 15) {
     score++;
     reasons.push("Strong ROE");
+  } else {
+    reasons.push("Weak ROE");
   }
+} else {
+  reasons.push("ROE not available");
+}
 
+if (debt > 0) {
   if (debt < 0.5) {
     score++;
     reasons.push("Low Debt");
+  } else {
+    reasons.push("High Debt");
   }
+} else {
+  reasons.push("Debt data missing");
+}
 
   // ===== VOLUME =====
   if (volume > 1000000) {
